125dd4c0db023ae9626cc4deb29d4fded384c2f9
taichi221228 <taichi221228@icloud.com>
2023-10-19 21:46:45 +0900

    Add helper for `DeferredRegister`

---
+++ src/main/java/taichi221228/golden_age/util/DeferredRegisterHelper.java
@@ -0,0 +1,22 @@
+package taichi221228.golden_age.util;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.IForgeRegistry;
+import taichi221228.golden_age.GoldenAge;
+
+public class DeferredRegisterHelper {
+    public static <T> DeferredRegister<T> createDeferredRegister(IForgeRegistry<T> registryType) {
+        return DeferredRegister.create(registryType, GoldenAge.MODID);
+    }
+
+    public static DeferredRegister<Block> createDeferredBlockRegister() {
+        return createDeferredRegister(ForgeRegistries.BLOCKS);
+    }
+
+    public static DeferredRegister<Item> createDeferredItemRegister() {
+        return createDeferredRegister(ForgeRegistries.ITEMS);
+    }
+}
