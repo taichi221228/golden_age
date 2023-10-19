2e0d149bcc9c8dd48064489ce5e9d1026a5e23f0
taichi221228 <taichi221228@icloud.com>
2023-10-20 00:21:56 +0900

    Add superclass for `ModdedBlock`

---
+++ src/main/java/taichi221228/golden_age/block/ModdedBlock.java
@@ -0,0 +1,29 @@
+package taichi221228.golden_age.block;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import taichi221228.golden_age.util.DeferredRegisterHelper;
+
+import java.util.function.Supplier;
+
+public class ModdedBlock {
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegisterHelper.createDeferredBlockRegister();
+    private static final DeferredRegister<Item> ITEMS = DeferredRegisterHelper.createDeferredItemRegister();
+
+    protected RegistryObject<Block> block;
+    protected RegistryObject<Item> blockItem;
+
+    public ModdedBlock(String name, Supplier<Block> blockSupplier) {
+        this.block = BLOCKS.register(name, blockSupplier);
+        this.blockItem = ITEMS.register(name, () -> new BlockItem(this.block.get(), new Item.Properties()));
+    }
+
+    public void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+        ITEMS.register(eventBus);
+    }
+}
