bdd68a8bd788c13dae8b7c0ba9ef73affca4a9ce
taichi221228 <taichi221228@icloud.com>
2023-10-20 12:25:34 +0900

    Refactor registration using lists and loops in `Register`

--- src/main/java/taichi221228/golden_age/Registrar.java
+++ src/main/java/taichi221228/golden_age/Registrar.java
@@ -1,13 +1,19 @@
 package taichi221228.golden_age;
 
 import net.minecraftforge.eventbus.api.IEventBus;
+import taichi221228.golden_age.block.ModdedBlock;
 import taichi221228.golden_age.block.ore.DeepslateTinOre;
 import taichi221228.golden_age.block.ore.TinOre;
 
+import java.util.Arrays;
+import java.util.List;
 
 public class Registrar {
+    private static final List<ModdedBlock> moddedBlocks = Arrays.asList(new DeepslateTinOre(), new TinOre());
+
     public static void registerAll(IEventBus eventBus) {
-        (new DeepslateTinOre()).register(eventBus);
-        (new TinOre()).register(eventBus);
+        for (ModdedBlock moddedBlock : moddedBlocks) {
+            moddedBlock.register(eventBus);
+        }
     }
 }
