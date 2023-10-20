db970a8362366685e68905fe9310c46511b4dae4
taichi221228 <taichi221228@icloud.com>
2023-10-20 12:17:01 +0900

    Add `Deepslate Tin Ore`

--- src/main/java/taichi221228/golden_age/Registrar.java
+++ src/main/java/taichi221228/golden_age/Registrar.java
@@ -1,11 +1,13 @@
 package taichi221228.golden_age;
 
 import net.minecraftforge.eventbus.api.IEventBus;
+import taichi221228.golden_age.block.ore.DeepslateTinOre;
 import taichi221228.golden_age.block.ore.TinOre;
 
 
 public class Registrar {
     public static void registerAll(IEventBus eventBus) {
+        (new DeepslateTinOre()).register(eventBus);
         (new TinOre()).register(eventBus);
     }
 }
---
+++ src/main/java/taichi221228/golden_age/block/ore/DeepslateTinOre.java
@@ -0,0 +1,13 @@
+package taichi221228.golden_age.block.ore;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import taichi221228.golden_age.block.ModdedBlock;
+
+public class DeepslateTinOre extends ModdedBlock {
+    public DeepslateTinOre() {
+        super("deepslate_tin_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DIRT)));
+    }
+}
+
