09047827b2a17e5301cd53a643ee4b888d59a2e0
taichi221228 <taichi221228@icloud.com>
2023-10-20 12:27:30 +0900

    Add `Tin Block`

--- src/main/java/taichi221228/golden_age/Registrar.java
+++ src/main/java/taichi221228/golden_age/Registrar.java
@@ -3,13 +3,14 @@ package taichi221228.golden_age;
 import net.minecraftforge.eventbus.api.IEventBus;
 import taichi221228.golden_age.block.ModdedBlock;
 import taichi221228.golden_age.block.ore.DeepslateTinOre;
+import taichi221228.golden_age.block.ore.TinBlock;
 import taichi221228.golden_age.block.ore.TinOre;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Registrar {
-    private static final List<ModdedBlock> moddedBlocks = Arrays.asList(new DeepslateTinOre(), new TinOre());
+    private static final List<ModdedBlock> moddedBlocks = Arrays.asList(new DeepslateTinOre(), new TinBlock(), new TinOre());
 
     public static void registerAll(IEventBus eventBus) {
         for (ModdedBlock moddedBlock : moddedBlocks) {
---
+++ src/main/java/taichi221228/golden_age/block/ore/TinBlock.java
@@ -0,0 +1,13 @@
+package taichi221228.golden_age.block.ore;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import taichi221228.golden_age.block.ModdedBlock;
+
+public class TinBlock extends ModdedBlock {
+    public TinBlock() {
+        super("tin_block", () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DIRT)));
+    }
+}
+
