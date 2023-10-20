8e7bc4673c012b47b6d199395261963a158478b2
taichi221228 <taichi221228@icloud.com>
2023-10-20 11:34:48 +0900

    Simplify the register in `Registrar`

--- src/main/java/taichi221228/golden_age/Registrar.java
+++ src/main/java/taichi221228/golden_age/Registrar.java
@@ -6,7 +6,6 @@ import taichi221228.golden_age.block.ore.TinOre;
 
 public class Registrar {
     public static void registerAll(IEventBus eventBus) {
-        final TinOre tinOre = new TinOre();
-        tinOre.register(eventBus);
+        (new TinOre()).register(eventBus);
     }
 }
