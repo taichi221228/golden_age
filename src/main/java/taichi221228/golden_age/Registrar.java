d38501c70bc6f47b2eba2be6746de1f2cab34235
taichi221228 <taichi221228@icloud.com>
2023-10-20 11:25:42 +0900

    Add `Tin Ore`

--- src/main/java/taichi221228/golden_age/Registrar.java
+++ src/main/java/taichi221228/golden_age/Registrar.java
@@ -1,8 +1,12 @@
 package taichi221228.golden_age;
 
 import net.minecraftforge.eventbus.api.IEventBus;
+import taichi221228.golden_age.block.ore.TinOre;
+
 
 public class Registrar {
     public static void registerAll(IEventBus eventBus) {
+        final TinOre tinOre = new TinOre();
+        tinOre.register(eventBus);
     }
 }
