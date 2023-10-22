464370cd8bff40fb1d51169f0cb15efb49d32583
taichi221228 <taichi221228@icloud.com>
2023-10-22 21:43:22 +0900

    Add `Raw Tin`

--- src/main/java/taichi221228/golden_age/Registrar.java
+++ src/main/java/taichi221228/golden_age/Registrar.java
@@ -6,16 +6,22 @@ import taichi221228.golden_age.block.ore.DeepslateTinOre;
 import taichi221228.golden_age.block.ore.RawTinBlock;
 import taichi221228.golden_age.block.ore.TinBlock;
 import taichi221228.golden_age.block.ore.TinOre;
+import taichi221228.golden_age.item.ModdedItem;
+import taichi221228.golden_age.item.ore.RawTin;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Registrar {
     private static final List<ModdedBlock> moddedBlocks = Arrays.asList(new DeepslateTinOre(), new RawTinBlock(), new TinBlock(), new TinOre());
+    private static final List<ModdedItem> moddedItems = Arrays.asList(new RawTin());
 
     public static void registerAll(IEventBus eventBus) {
         for (ModdedBlock moddedBlock : moddedBlocks) {
             moddedBlock.register(eventBus);
+        }
+        for (ModdedItem moddedItem : moddedItems) {
+            moddedItem.register(eventBus);
         }
     }
 }
---
+++ src/main/java/taichi221228/golden_age/item/ore/RawTin.java
@@ -0,0 +1,9 @@
+package taichi221228.golden_age.item.ore;
+
+import taichi221228.golden_age.item.ModdedItem;
+
+public class RawTin extends ModdedItem {
+    public RawTin() {
+        super("raw_tin");
+    }
+}
