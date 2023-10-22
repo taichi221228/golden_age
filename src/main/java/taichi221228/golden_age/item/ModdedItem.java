612edbc874d5f26f55e805c5b8fee0d1f6e089f4
taichi221228 <taichi221228@icloud.com>
2023-10-22 23:27:53 +0900

    Add constructor to `ModdedItem` to accept custom item supplier

--- src/main/java/taichi221228/golden_age/item/ModdedItem.java
+++ src/main/java/taichi221228/golden_age/item/ModdedItem.java
@@ -6,6 +6,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import taichi221228.golden_age.util.DeferredRegisterHelper;
 
+import java.util.function.Supplier;
+
 public class ModdedItem {
     private final DeferredRegister<Item> ITEMS = DeferredRegisterHelper.createDeferredItemRegister();
 
@@ -13,6 +15,10 @@ public class ModdedItem {
 
     public ModdedItem(String name) {
         this.item = ITEMS.register(name, () -> new Item(new Item.Properties()));
+    }
+
+    public ModdedItem(String name, Supplier<Item> itemSupplier) {
+        this.item = ITEMS.register(name, itemSupplier);
     }
 
     public void register(IEventBus eventBus) {
