80c876142c8a2f0eb0508e2862e14eaa3631706f
taichi221228 <taichi221228@icloud.com>
2023-10-23 20:52:21 +0900

    Add tag for `need_copper_tool`

---
+++ src/main/java/taichi221228/golden_age/tags/NeedCopperTool.java
@@ -0,0 +1,9 @@
+package taichi221228.golden_age.tags;
+
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+import taichi221228.golden_age.util.TagsHelper;
+
+public class NeedCopperTool {
+    public static final TagKey<Block> NEED_COPPER_TOOL = TagsHelper.createBlockTags("need_copper_tool");
+}
